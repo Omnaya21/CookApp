50d5eb6b385cf5d7b71237ba87298b01a8afb3de
Omar Navarro <omar.navarro@ingyso.com>
2018-10-30 06:31:44 -0700

    - Splash screen made full screen.

--- app/src/main/java/com/ktchen/cookapp/Splash.java
+++ app/src/main/java/com/ktchen/cookapp/Splash.java
@@ -2,12 +2,18 @@ package com.ktchen.cookapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  // Hide the title
+        getSupportActionBar().hide();   // Hide the title bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); // Enable ful screen mode
         setContentView(R.layout.activity_splash);
     }
 }
