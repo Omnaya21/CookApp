2ff0acc62e52bb508f53985649075b0c6d4a5c03
J.Ortner <44012766+jornter1222@users.noreply.github.com>
2018-10-30 17:50:08 -0700

    added comment

--- app/src/main/java/com/ktchen/cookapp/Splash.java
+++ app/src/main/java/com/ktchen/cookapp/Splash.java
@@ -9,6 +9,7 @@ import android.view.WindowManager;
 // Another comment fro practice
 // Jess made a comment here.
 // Omar's comment
+// Made a comment here
 public class Splash extends AppCompatActivity {
 
     @Override
