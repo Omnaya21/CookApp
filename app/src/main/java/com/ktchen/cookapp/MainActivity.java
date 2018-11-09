12f2bc9bfdd2865c44c84768d6e6c47c594a3078
Omar Navarro <Omar@server.ingyso.private>
2018-11-09 14:51:56 -0800

    Added onclick events for the main options

--- app/src/main/java/com/ktchen/cookapp/MainActivity.java
+++ app/src/main/java/com/ktchen/cookapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -13,7 +14,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     public static ListView optionsList;
-    public static ArrayList<String> options;
+    String[] options = new String[]{"Recipes", "Calendar", "Shopping List",
+            "Add New Recipe", "Make a Plan"};
     public static ArrayAdapter<String> optionsAdapter;
 
     @Override
@@ -21,15 +23,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Add optins to ListView
         optionsList = findViewById(R.id.option_menu_list);
-        options = new ArrayList<>();
-        options.add("Recipes");
-        options.add("Calendar");
-        options.add("Shopping List");
-        options.add("Add New Recipe");
-        options.add("Make a Plan");
         optionsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, options);
         optionsList.setAdapter(optionsAdapter);
+
+        //Add onClick events to list options
+        optionsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position) {
+                    case 0: // Recipes option
+
+                        break;
+                    case 1: // Calendar option
+
+                        break;
+                    case 2: // Shopping List option
+
+                        break;
+                    case 3: // Add New Recipe option
+                        Intent i = new Intent(view.getContext(), AddRecipe.class);
+                        startActivityForResult(i, 0);
+                        break;
+                    case 4: // Make a Plan option
+
+                        break;
+                }
+
+
+
+            }
+        });
 
         TextView label = findViewById(R.id.title);
         label.setVisibility(View.INVISIBLE);
