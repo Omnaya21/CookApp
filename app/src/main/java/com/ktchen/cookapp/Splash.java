6dfe55c7c91c0e19d855c20c93dc50d2f6f94d79
J.Ortner <44012766+jornter1222@users.noreply.github.com>
2018-11-09 09:56:03 -0800

    fixed possible null pointer in splash and changed minSdk

--- app/src/main/java/com/ktchen/cookapp/Splash.java
+++ app/src/main/java/com/ktchen/cookapp/Splash.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import java.util.Objects;
+
 /// this is our first screen we show to users
 // Another comment fro practice
 // Jess made a comment here.
@@ -17,7 +19,7 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);  // Hide the title
-        getSupportActionBar().hide();   // Hide the title bar
+        Objects.requireNonNull(getSupportActionBar()).hide();   // Hide the title bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); // Enable ful screen mode
         setContentView(R.layout.activity_splash);
