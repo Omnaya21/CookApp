7469f2536c5bc717bd585845572e2b94e36327e5
J.Ortner <44012766+jornter1222@users.noreply.github.com>
2018-11-08 20:11:19 -0800

    skeleton for recipe class

--- app/src/main/java/com/ktchen/cookapp/recipe.java
+++ app/src/main/java/com/ktchen/cookapp/recipe.java
@@ -1,4 +1,68 @@
 package com.ktchen.cookapp;
 
+import android.media.Image;
+import android.provider.CalendarContract;
+
+import java.util.List;
+
 public class recipe {
+
+    private String name;
+    private Image picture;
+    private List<ingredient> ingredients;
+    private List<String> directions;
+    private List<CalendarContract.Reminders> reminders;
+
+    recipe(String name){
+       this.name=name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Image getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Image picture) {
+        this.picture = picture;
+    }
+
+    public List<ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getDirections() {
+        return directions;
+    }
+
+    public void setDirections(List<String> directions) {
+        this.directions = directions;
+    }
+
+    public List<CalendarContract.Reminders> getReminders() {
+        return reminders;
+    }
+
+    public void setReminders(List<CalendarContract.Reminders> reminders) {
+        this.reminders = reminders;
+    }
+    public void addItem (List<recipe> item) {
+
+    }
+    public void removeItem (List<recipe> item) {
+
+    }
+    public void clearList (List<recipe> list){
+
+    }
 }
