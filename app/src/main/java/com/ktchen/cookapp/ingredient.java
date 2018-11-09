6f18e9af12f578975081bcea0afeec7b8baae673
J.Ortner <44012766+jornter1222@users.noreply.github.com>
2018-11-08 20:15:39 -0800

    ingredient class with non default constructor and getters and setters.

--- app/src/main/java/com/ktchen/cookapp/ingredient.java
+++ app/src/main/java/com/ktchen/cookapp/ingredient.java
@@ -1,4 +1,37 @@
 package com.ktchen.cookapp;
 
 public class ingredient {
+    private String name;
+    private float quantity;
+    private String unit;
+
+    public ingredient(String name, float quantity, String unit) {
+        this.name = name;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(float quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
 }
