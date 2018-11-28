799673131968847aacc907bb75611efa46df9bf3
J.Ortner <44012766+jornter1222@users.noreply.github.com>
2018-11-27 18:15:36 -0800

    javadoc for  DatabaseHelper

--- app/src/main/java/com/ktchen/cookapp/database/DatabaseHelper.java
+++ app/src/main/java/com/ktchen/cookapp/database/DatabaseHelper.java
@@ -18,6 +18,13 @@ import static com.ktchen.cookapp.database.model.recipeTable.COLUMN_INGREDIENT;
 import static com.ktchen.cookapp.database.model.recipeTable.COLUMN_TITLE;
 import static com.ktchen.cookapp.database.model.recipeTable.TABLE_NAME;
 
+/**
+ * This class translates functions into SQL commands.
+ *
+ * It extends SQLiteOpenHelper, and allows user to do things like insert stuff in the database.
+ *
+ * @author Jessica Ortner
+ */
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION =3;
     private static final String DATABASE_NAME = "recipes_db";
@@ -26,17 +33,36 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME,null,DATABASE_VERSION);
 
     }
+
+    /**
+     * Creates the database.
+     *
+     * @param db Passes in a SQL Database.
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(recipeTable.CREATE_TABLE);
     }
 
+    /**
+     * Upgrades the database.
+     *
+     * @param db Passes in the Database.
+     * @param oldVersion An int representing the old version number.
+     * @param newVersion An int representign new version number.
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS "+ recipeTable.TABLE_NAME);
         onCreate(db);
     }
 
+    /**
+     * Inserts a recipe into the database.
+     *
+     * @param recipe Takes a Recipe as the parameter.
+     * @return returns a long representing an id.
+     */
     public long insertRecipe(Recipe recipe){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
@@ -51,6 +77,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return id;
     }
 
+    /**
+     * Gets a recipe from the database.
+     *
+     * @param id Takes a long that represents the id.
+     * @return Returns the recipe.
+     */
     public Recipe getRecipe(long id){
      SQLiteDatabase db = this.getReadableDatabase();
      Cursor cursor = db.query(TABLE_NAME,
@@ -70,6 +102,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     return recipe;
     }
 
+    /**
+     * Gets all the Recipes in the database.
+     *
+     * @return Returns a list containing of all the recipes.
+     */
     public List<Recipe> getAllRecipes() {
         List<Recipe> recipes = new ArrayList<Recipe>();
         String selectQuery = "SELECT * FROM "+ TABLE_NAME + " ORDER BY " +COLUMN_TITLE + " DESC ";
@@ -90,7 +127,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             db.close();
         return recipes;
         }
- public int updateRecipe(Recipe recipe){
+
+    /**
+     * Updates the recipe in the database.
+     * @param recipe Takes the new version of the recipe.
+     * @return Returns ID of the recipe.
+     */
+    public int updateRecipe(Recipe recipe){
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -101,6 +144,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return db.update(TABLE_NAME, values, COLUMN_ID + " =?", new String[] {String.valueOf(recipe.getID())});
  }
 
+    /**
+     * Deletes a recipe.
+     *
+     * @param recipe Takes the recipe to be deleted.
+     */
  public void deleteRecipe(Recipe recipe){
         SQLiteDatabase db= this.getWritableDatabase();
         db.delete(TABLE_NAME, COLUMN_ID + " =?",
