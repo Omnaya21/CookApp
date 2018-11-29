aaa5cd9f47cf78ab626db08ee1b3bc45c31cda70
J.Ortner <44012766+jornter1222@users.noreply.github.com>
2018-11-28 21:55:11 -0800

    more starting calendar

--- app/src/main/java/com/ktchen/cookapp/ViewCalendar.java
+++ app/src/main/java/com/ktchen/cookapp/ViewCalendar.java
@@ -1,11 +1,25 @@
 package com.ktchen.cookapp;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class ViewCalendar extends AppCompatActivity {
+    CalendarView calendarView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        calendarView= (CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), dayOfMonth +"/"+ month + "/" + year, Toast.LENGTH_LONG);
+
+            }
+        });
     }
 }
 
