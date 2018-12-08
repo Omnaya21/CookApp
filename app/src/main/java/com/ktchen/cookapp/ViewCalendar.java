d6df83d53e8bcc3e2d95dff45341ba46a0798c75
J.Ortner <44012766+jornter1222@users.noreply.github.com>
2018-12-08 13:26:27 -0800

    added date change listener to calendar.

--- app/src/main/java/com/ktchen/cookapp/ViewCalendar.java
+++ app/src/main/java/com/ktchen/cookapp/ViewCalendar.java
@@ -3,6 +3,7 @@ package com.ktchen.cookapp;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
@@ -15,11 +16,22 @@ import java.util.List;
 
 public class ViewCalendar extends AppCompatActivity {
     CalendarView calendarView;
-
+    private static final String TAG = "calendar view";
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
         // Get a reference for the week view in the layout.
+        calendarView= (CalendarView) findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date= month+1 + "/"+dayOfMonth + "/"+ year;
+                Log.i(TAG, "date selected is "+ date);
+            }
+        });
+
+
     }
 }
